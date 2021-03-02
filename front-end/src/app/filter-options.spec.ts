1524bd5a588f55b53b6189fcae281cf08db38b1e
Zach Miles <zachmiles@C02DQ7N1ML7H.local>
2021-03-02 16:43:57 -0500

    accepting data from backend and rendering table

--- front-end/src/app/filter-options.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FilterOptions } from './filter-options';
-
-describe('FilterOptions', () => {
-  it('should create an instance', () => {
-    expect(new FilterOptions()).toBeTruthy();
-  });
-});
